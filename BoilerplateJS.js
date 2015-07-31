a35aaaf93d8caa03ad4bbd38739f1e0d541ae4fc
lomasnz <paul.armstrong.nz@gmail.com>
2015-07-31 08:21:47 +0000

    friday

---
+++ BoilerplateJS.js
@@ -0,0 +1,62 @@
+// JavaScript File
+
+/**
+ * Funtion returns a json list of bolerplateSets. Containig attributes
+ * name : string
+ * description: string 
+ */
+function _retriveBoilerplateSets() {
+    var result = [{
+        name: 'Corporate',
+        description: 'Bolierplates'
+    }, {
+        name: 'HR',
+        description: 'Boilerplates'
+    }, {
+        name: 'Marketing',
+        description: 'Boilerplates'
+    }];
+    return result;
+}
+/**
+ * Function return json list of Bolierplates in a Boilerplate Set of the give setName passed.
+ * Containig attributes
+ * name : string
+ * description: string 
+ */
+function _retreieveBoilerplatesForSet(setName) {
+    var result;
+
+    result = [{
+        name: 'My CV',
+        description: 'Boilerplate'
+    }, {
+        name: 'Jain CV',
+        description: 'Boilerplate'
+    }, {
+        name: 'Joe CV',
+        description: 'Boilerplate'
+    }, {
+        name: 'Jeff CV',
+        description: 'Boilerplate'
+    }, {
+        name: 'Jack CV',
+        description: 'Boilerplate'
+    }, {
+        name: 'Jill CV',
+        description: 'Boilerplate'
+    }, {
+        name: 'Jon CV',
+        description: 'Boilerplate'
+    }, {
+        name: 'Jo CV',
+        description: 'Boilerplate'
+    }, {
+        name: 'John CV',
+        description: 'Boilerplate'
+    }, {
+        name: 'Jedd CV',
+        description: 'Boilerplate'
+    }];
+    return result;
+}
